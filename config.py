7896fb7c50fa9fb173f0d51d8c602a293fa5243e
Michael Langford <michael.langford29@gmail.com>
2020-02-21 17:58:22 -0600

    many changes to tpot ensemble test

--- config.py
+++ config.py
@@ -2,586 +2,63 @@
 
 
 # a config for the run itself
-# we'll assume the model_config is true! Thanks
 config = {
 	
 	# number of 'hidden' layers. Probably not the right term
 	# but these are the layers that are in between the base and the meta-learner
 	'num_hidden_layers':(1, 1),
 
-	# number of bayesian opt iterations we'll train each model for
+	# number of models in the base. This is a range
+	'num_base':(10, 50),
+
+	# number of models in hidden layers. This is a range
+	'num_hidden':(5, 10),
+
+	# number of voters. For now we'll have this as just an int
+	'num_voters':5,
+
+	# are we going to do feature elimination based on correlation?
+	'correlation_feature_elimination':True,
+
+	# how many iterations for Boruta to run
+	'max_iter_boruta':1000,
+
+	# number of bayesian opt iterations we'll optimize voting weights for
 	'meta_learner_its':100,
+
+	# what metric will we be using to evaluate?
 	'metric':'f1',
 
-}
+	# base TPOT parameters
+	'base_num_gens':2,
+	'base_pop_size':2,
+	'base_cv':2,
 
-# more thoughts
-# what if we want to lock down a hyperparameter at a value
-# we're not trying to optimize it, but we just don't want to use the default sklearn value?
-# maybe we'll have a key in the hyperparameter of "value"
-# then we'll check to see if value is present in the hparam's keys
-# if it is, then we need to just use that value and we're not optimizing for it
-# we'll just add support for that but not necessarily use it right now
+	# hidden TPOT parameters
+	'hidden_num_gens':2,
+	'hidden_pop_size':2,
+	'hidden_cv':2,
 
-# then a dictionary for the models we'll be using and the actual ranges we'll use
-# and which hyperparameters we'll be tuning for each one of the models
-##### MAYBE HAVE SUPPORT FOR 'LEARNER' BOOL SO THAT YOU CAN EXCLUDE SOME FROM BEING BASE LEARNERS
-model_config = {
+	# voting TPOT parameters
+	'voting_num_gens':20,
+	'voting_pop_size':20,
+	'voting_cv':5,
 
-	'LogisticRegression':{
-		'metalearner':True,
+	# number of cv folds we use while training the whole ensemble
+	'num_folds':5
 
-		# RANGE DOES NOT INCLUDE UPPER BOUND
-		'num_in_base':(0, 10),
-		'num_in_mid':(0, 5),
-		'hyperparameters_for_base':{
-			'tol':{
-				'range':'base'
-			},
-			'C':{
-				'range':'base'
-			}
-		},
-
-		# these are the hyperparameter ranges for when they're not a base model
-		'hyperparameters':{
-			'tol':{
-				'range':'base'
-			},
-			'C':{
-				'range':'base'
-			}
-		}
-	},
-
-	'KNeighborsClassifier':{
-		'metalearner':False,
-		'num_in_base':(0, 10),
-		'num_in_mid':(0, 5),
-		'hyperparameters_for_base':{
-			'n_neighbors':{
-				'range':(1, 20)
-			},
-			'p':{
-				'range':(1, 5)
-			}
-		},
-		'hyperparameters':{
-			'n_neighbors':{
-				'range':(1, 100)
-			},
-			'p':{
-				'range':(1, 5)
-			}
-		}
-	},
-
-	'GaussianNB':{
-		'metalearner':False,
-		'num_in_base':(0, 2),
-		'num_in_mid':(0, 2),
-		'hyperparameters_for_base':{},
-		'hyperparameters':{}
-	},
-
-	'SVC':{
-		'metalearner':False,
-		'num_in_base':(0, 10),
-		'num_in_mid':(0, 5),
-		'hyperparameters_for_base':{
-			'C':{
-				'range':'base'
-			},
-			'tol':{
-				'range':'base'
-			},
-			'probability':True,
-			'max_iter':1000
-		},
-		'hyperparameters':{
-			'C':{
-				'range':'base'
-			},
-			'tol':{
-				'range':'base'
-			},
-			'probability':True
-		}
-	},
-
-	'ExtraTreesClassifier':{
-		'metalearner':True,
-		'num_in_base':(0, 10),
-		'num_in_mid':(0, 5),
-		'hyperparameters_for_base':{
-			'n_estimators':{
-				'range':(2, 20)
-			},
-			'max_depth':{
-				'range':(1, 5)
-			}
-		},
-		'hyperparameters':{
-			'n_estimators':{
-				'range':(10, 500)
-			},
-			'max_depth':{
-				'range':(2, 10)
-			}
-		}
-	},
-
-	'RandomForestClassifier':{
-		'metalearner':True,
-		'num_in_base':(0, 10),
-		'num_in_mid':(0, 5),
-		'hyperparameters_for_base':{
-			'n_estimators':{
-				'range':(2, 20)
-			},
-			'max_depth':{
-				'range':(1, 5)
-			},
-		},
-		'hyperparameters':{
-			'n_estimators':{
-				'range':(10, 500)
-			},
-			'max_depth':{
-				'range':(1, 10)
-			},
-		}
-	},
-
-	'AdaBoostClassifier':{
-		'metalearner':True,
-		'num_in_base':(0, 10),
-		'num_in_mid':(0, 5),
-		'hyperparameters_for_base':{
-			'n_estimators':{
-				'range':(2, 20)
-			},
-			'learning_rate':{
-				'range':'base'
-			}
-		},
-		'hyperparameters':{
-			'n_estimators':{
-				'range':(10, 500)
-			},
-			'learning_rate':{
-				'range':'base'
-			}
-		}
-	},
-
-	# 'Lasso':{
-	# 	'metalearner':False,
-	# 	'num_in_base':(0, 10),
-	# 	'num_in_mid':(0, 5),
-	# 	'hyperparameters_for_base':{
-	# 		'alpha':{
-	# 			'range':'base'
-	# 		},
-	# 		'tol':{
-	# 			'range':'base'
-	# 		}
-	# 	},
-	# 	'hyperparameters':{
-	# 		'alpha':{
-	# 			'range':'base'
-	# 		},
-	# 		'tol':{
-	# 			'range':'base'
-	# 		}
-	# 	}
-	# },
-
-	# 'Ridge':{
-	# 	'metalearner':False,
-	# 	'num_in_base':(0, 10),
-	# 	'num_in_mid':(0, 5),
-	# 	'hyperparameters_for_base':{
-	# 		'alpha':{
-	# 			'range':'base'
-	# 		},
-	# 		'tol':{
-	# 			'range':'base'
-	# 		}
-	# 	},
-	# 	'hyperparameters':{
-	# 		'alpha':{
-	# 			'range':'base'
-	# 		},
-	# 		'tol':{
-	# 			'range':'base'
-	# 		}
-	# 	}
-	# },
-
-	# 'ElasticNet':{
-	# 	'metalearner':False,
-	# 	'num_in_base':(0, 10),
-	# 	'num_in_mid':(0, 5),
-	# 	'hyperparameters_for_base':{
-	# 		'alpha':{
-	# 			'range':'base'
-	# 		},
-	# 		'l1_ratio':{
-	# 			'range':'base'
-	# 		}
-	# 	},
-	# 	'hyperparameters':{
-	# 		'alpha':{
-	# 			'range':'base'
-	# 		},
-	# 		'l1_ratio':{
-	# 			'range':'base'
-	# 		}
-	# 	}
-	# },
-
-#	'GaussianProcessClassifier':{
-#		'metalearner':True,
-#		'num_in_base':(0, 5),
-#		'num_in_mid':(0, 3),
-#		'hyperparameters_for_base':{
-#			'max_iter_predict':{
-#				'type':'int',
-#				'range':'base'
-#			},
-#			'n_restarts_optimizer':{
-#				'type':'int',
-#				'range':'base'
-#			}
-#		},
-#		'hyperparameters':{
-#
-#			'max_iter_predict':{
-#				'type':'int',
-#				'range':'base'
-#			},
-#			'n_restarts_optimizer':{
-#				'type':'int',
-#				'range':'base'
-#			}
-#		}
-#	},
-
-	'DecisionTreeClassifier':{
-		'metalearner':False,
-		'num_in_base':(0, 50),
-		'num_in_mid':(0, 5),
-		'hyperparameters_for_base':{
-			'max_depth':{
-				'type':'int',
-				'range':'base'
-			}
-		},
-		'hyperparameters':{
-
-			'max_depth':{
-				'type':'int',
-				'range':'base'
-			}
-
-			# maybe have a categorical variable for criterion
-		}
-	}
-
-#	'MLPClassifier':{
-#		'metalearner':True,
-#		'num_in_base':(0, 5),
-#		'num_in_mid':(0, 3),
-#		'hyperparameters_for_base':{
-#			'n_estimators':{
-#				'range':(2, 20)
-#			},
-#			'learning_rate':{
-#				'range':'base'
-#			}
-#		},
-#		'hyperparameters':{
-#
-#			# not going to use ACTUAL hyperparameters here
-#			# this will require some redoing in the actual script to make it work
-#
-#			'num_layers':{
-#				'type':'int',
-#				'default_range':(1, 10)
-#			},
-#
-#			'hidden_layer_size':{
-#				'type':'int',
-#				'default_range':(2, 100)
-#			},
-#
-#			'max_iter':{
-#				'type':'int',
-#				'default_range':(1, 10)
-#			}
-#		}
-#	}
-}
-
-
-# config to place all of the models we *can* use
-# for now, just classification
-classification_models = {
-	
-	'XGBoost':{
-		'alias':'xgb',
-		'import_statement':'import XGBoost',
-		'hyperparameters':{
-
-			# in here we should include what the default or recommended range is for the optimization
-			'n_estimators':{
-				'type':'int',
-				'default_range':(5, 500)
-			},
-			'eta':{
-				'type':'float',
-				'default_range':(0.0001, 0.5)
-			},
-			'max_depth':{
-				'type':'int',
-				'default_range':(1, 12)
-			},
-			'subsample':{
-				'type':'float',
-				'default_range':(0.01, 0.99)
-			},
-			'scale_pos_weight':{
-				'type':'float',
-				'default_range':(1, 10000)
-			}
-		}
-	},
-
-	'LogisticRegression':{
-		'alias':'lr',
-		'import_statement':'from sklearn.linear_model import LogisticRegression',
-		'hyperparameters':{
-
-			'tol':{
-				'type':'float',
-				'default_range':(0.000001, 0.01)
-			},
-			'C':{
-				'type':'float',
-				'default_range':(0.00001, 10)
-			}
-		}
-	},
-
-	'KNeighborsClassifier':{
-		'alias':'knn',
-		'import_statement':'from sklearn.neighbors import KNeighborsClassifier',
-		'hyperparameters':{
-
-			'n_neighbors':{
-				'type':'int',
-				'default_range':(2, 100)
-			},
-			'p':{
-				'type':'int',
-				'default_range':(1, 5)
-			}
-		}
-	},
-
-	'GaussianNB':{
-		'alias':'nb',
-		'import_statement':'from sklearn.naive_bayes import GaussianNB',
-		##### there are no hyperparameters for NB!
-		# we can probably hard code this one doesn't need to be optimized, just used
-		'hyperparameters':{}
-	},
-
-	'SVC':{
-		'alias':'svc',
-		'import_statement':'from sklearn.svm import SVC',
-		'hyperparameters':{
-
-			'C':{
-				'type':'float',
-				'default_range':(0.1, 20)
-			},
-			'tol':{
-				'type':'float',
-				'default_range':(0.00001, 0.001)
-			},
-			'probability':True,
-			'max_iter':100000
-		}
-	},
-
-	'ExtraTreesClassifier':{
-
-		'alias':'etc',
-		'import_statement':'from sklearn.ensemble import ExtraTreesClassifier',
-		'hyperparameters':{
-			'n_estimators':{
-				'type':'int',
-				'default_range':(5, 500)
-			},
-			'max_depth':{
-				'type':'int',
-				'default_range':(2, 12)
-			},
-			'scale_pos_weight':{
-				'type':'float',
-				'default_range':(1, 10000)
-			}
-		}
-	},
-
-	'RandomForestClassifier':{
-
-		'alias':'rfc',
-		'import_statement':'from sklearn.ensemble import RandomForestClassifier',
-		'hyperparameters':{
-
-			# in here we should include what the default or recommended range is for the optimization
-			'n_estimators':{
-				'type':'int',
-				'default_range':(1,500)
-			},
-			'max_depth':{
-				'type':'int',
-				'default_range':(1, 12)
-			}
-		}
-	},
-
-	'AdaBoostClassifier':{
-
-		'alias':'ada',
-		'import_statement':'from sklearn.ensemble import AdaBoostClassifier',
-		'hyperparameters':{
-
-			'learning_rate':{
-				'type':'float',
-				# changing this one because there's a trade-off between learning rate and n_est
-				'default_range':(0.001, 2)
-			},
-			'n_estimators':{
-				'type':'int',
-				'default_range':(1,500)
-			}
-		}
-	},
-
-	'Lasso':{
-
-		'alias':'lasso',
-		'import_statement':'from sklearn.linear_model import Lasso',
-		'hyperparameters':{
-
-			'alpha':{
-				'type':'float',
-				'default_range':(0.01, 2)
-			},
-			'tol':{
-				'type':'float',
-				'default_range':(.00001,.01)
-			}
-		}
-	},
-
-	'Ridge':{
-
-		'alias':'ridge',
-		'import_statement':'from sklearn.linear_model import Ridge',
-		'hyperparameters':{
-
-			'alpha':{
-				'type':'float',
-				'default_range':(0.01,2)
-			},
-			'tol':{
-				'type':'float',
-				'default_range':(.00001,.01)
-			}
-		}
-	},
-
-	'ElasticNet':{
-
-		'alias':'enet',
-		'import_statement':'from sklearn.linear_model import ElasticNet',
-		'hyperparameters':{
-
-			'alpha':{
-				'type':'float',
-				'default_range':(0.01,2)
-			},
-			'l1_ratio':{
-				'type':'float',
-				'default_range':(.1,.9)
-			}
-		}
-	},
-
-	'GaussianProcessClassifier':{
-
-		'alias':'gpc',
-		'import_statement':'from sklearn.gaussian_process import GaussianProcessClassifier',
-		'hyperparameters':{
-
-			'max_iter_predict':{
-				'type':'int',
-				'default_range':(10,1000)
-			},
-			'n_restarts_optimizer':{
-				'type':'int',
-				'default_range':(0,5)
-			},
-		}
-	},
-
-	'DecisionTreeClassifier':{
-
-		'alias':'dtc',
-		'import_statement':'from sklearn.tree import DecisionTreeClassifier',
-		'hyperparameters':{
-
-			'max_depth':{
-				'type':'int',
-				'default_range':(1, 10)
-			}
-
-			# maybe have a categorical variable for criterion
-		}
-	}
-
-#	'MLPClassifier':{
-#
-#		'alias':'mlp',
-#		'import_statement':'from sklearn.neural_network import MLPClassifier',
-#		'hyperparameters':{
-#
-#			# not going to use ACTUAL hyperparameters here
-#			# this will require some redoing in the actual script to make it work
-#
-#			'num_layers':{
-#				'type':'int',
-#				'default_range':(1, 10)
-#			},
-#
-#			'hidden_layer_size':{
-#				'type':'int',
-#				'default_range':(2, 100)
-#			},
-#
-#			'max_iter':{
-#				'type':'int',
-#				'default_range':(1, 10)
-#			}
-#		}
-#	}
 
 }
 
+base_models = {
 
+}
+
+hidden_models = {
+
+}
+
+voting_models = {
+
+}
 
--- tpot_ensemble_test.py
+++ tpot_ensemble_test.py
@@ -1,83 +1,274 @@
-
-
 import numpy as np 
 import pandas as pd 
 import os
-from sklearn.datasets import load_digits
-from sklearn.model_selection import train_test_split
+from random import random
+import config
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import f1_score
+from sklearn.model_selection import train_test_split
+from sklearn.manifold import TSNE
+from sklearn.ensemble import VotingClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+#from sklearn.utils import shuffle
+#from sklearn.metrics import confusion_matrix
+#from sklearn.preprocessing import StandardScaler
+import featuretools as ft 
+import featuretools.variable_types as vtypes 
+from featuretools.primitives import make_agg_primitive, make_trans_primitive
+from featuretools.variable_types import Numeric
 
 from bayes_opt import BayesianOptimization
-
-import pandas as pd
-import numpy as np 
-import tensorflow as tf
-#from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
-from sklearn.metrics import confusion_matrix
-from sklearn.preprocessing import StandardScaler
 
 from mlens.ensemble import SuperLearner
 
 from tpot import TPOTClassifier
 
+from boruta import BorutaPy
 
-
+# can we axe the warnings?
+def warn(*args, **kwargs):
+	pass
+import warnings
+warnings.warn = warn
 
 df = pd.read_csv("creditcard.csv")
-
-df = df.drop(['Time','V28','V27','V26','V25','V24','V23','V22','V20','V15','V13','V8'], axis =1)
+df = df.drop(['Time'], axis=1) #,'V28','V27','V26','V25','V24','V23','V22','V20','V15','V13','V8'], axis =1)
 df = df.dropna()
 
+# before we get into things, let's do all the featuretools definitions
+def log_plus_one(column):
+	return np.log(column + min(column) + 1)
+lpo = make_trans_primitive(function=log_plus_one, input_types=[Numeric], return_type=Numeric)
 
-train_df = df.sample(frac=0.8, random_state=0)
-test_df =  df.drop(train_df.index)
+def abs_log(column):
+	return np.log(np.abs(column) + 1)
+al = make_trans_primitive(function=abs_log, input_types=[Numeric], return_type=Numeric)
 
-train_df = shuffle(train_df)
-test_df = shuffle(test_df)
+def squared(column):
+	return np.square(column)
+sq = make_trans_primitive(function=squared, input_types=[Numeric], return_type=Numeric)
 
-y_train = train_df.pop('Class').to_numpy()
-y_test = test_df.pop('Class').to_numpy()
-X_train = train_df.to_numpy()
-X_test = test_df.to_numpy()
+def add_cols(numeric1, numeric2):
+	return numeric1+numeric2
+adc = make_trans_primitive(function=add_cols, input_types=[Numeric, Numeric], return_type=Numeric)
+
+def add_abs_cols(numeric1, numeric2):
+	return np.abs(numeric1) + np.abs(numeric2)
+aac = make_trans_primitive(function=add_abs_cols, input_types=[Numeric, Numeric], return_type=Numeric)
+
+def sqrt_square_sum(numeric1, numeric2):
+	return np.sqrt(np.square(numeric1) + np.square(numeric2))
+sss = make_trans_primitive(function=sqrt_square_sum, input_types=[Numeric, Numeric], return_type=Numeric)
+
+### It would be interesting to see if having a third column in any of this changes things!
+
+# ok and then we'll do all the featuretools things that need to happen
+es = ft.EntitySet(id = 'card') # no clue what this means but whatever
+
+# make an entity from the observations data
+es = es.entity_from_database(dataframe = df.drop('Class', axis=1),
+								entity_id = 'obs',
+								index = 'index')
+
+feature_matrix, feature_names = ft.dfs(entityset=es, target_entity='obs',
+										agg_primitixes = ['min', 'max', 'mean', 'count', 'sum', 'std', 'trend'],
+										trans_primitives = ['percentile', sss], #['percentile', lpo, al, sq, adc, aac, sss],
+										max_depth=1,
+										n_jobs=-1,
+										verbose=1)
+
+# eliminate features if they're too correlated before we get into boruta
+if config.config.correlation_feature_elimination:
+	feature_matrix = feature_selection(feature_matrix, correlation_threshold = 0.95)
+	print()
+	print('Columns after feature engineering and correlation elimination:')
+	print(list(feature_matrix.columns))
+
+df_ = feature_matrix # make a copy of this
+y = df.pop('Class')
+
+
+# now let's do some boruta!!
+rfc = RandomForestClassifier(n_jobs = -1)
+boruta_selector = BorutaPy(rfc, n_estimators='auto', verbose=2, max_iter=config.config.max_iter_boruta)
+boruta_selector.fit(X.to_numpy(), y.to_numpy())
+
+print()
+print(' Number of selected features: {}'.format(boruta_selector.n_features_))
+
+# now go through and actually select those features
+total_cols = list(X.columns) # list of all the names
+chosen = list(boruta_selector.support_) # this is array of booleans
+chosen_features = [] # these are the NAMES of the ones we'll be using
+
+for i in range(len(total_cols)):
+	if chosen[i]:
+		chosen_features.append(total_cols[i])
+
+print()
+print(' Final chosen features:')
+print(' {}'.format(chosen_features))
+
+# split these out
+# this isn't going to be shuffled because that's a mess.
+X_train, X_test, y_train, y_test = train_test_split(X[chosen_features], y, test_size=0.25)
 
 
 # now let's make a bunch of lists of tpots
+
+# hold all the base trained pipelines
 base_list = []
-num_base = 2
 
+# list of lists to hold the hidden pipelines for each layer
+hidden_lol = []
 
-##### Train each of the models before you get to the super learner
-##### I think you want to export the model itself into a separate list or something
-##### like the whole pipeline it finds
+# let's choose some of these numbers that are going to be assigned randomly
+num_hidden_layers = random.randint(config.config.num_hidden_layers[0], config.config.num_hidden_layers[1])
+num_base = random.randint(config.config.num_base[0], config.config.num_base[1])
 
+print()
+print('Training {} base TPOT pipelines'.format(num_base))
 
-for i in range(num_base):
+for _ in range(num_base):
     
-    base_list.append(TPOTClassifier(generations=2, population_size=2, scoring="f1", cv=2, n_jobs=-1, verbosity=1).fit(X_train[0:10000,:], y_train[0:10000]).fitted_pipeline_)
+    base_list.append(TPOTClassifier(generations=config.config.base_num_gens, 
+    								population_size=config.config.base_pop_size, 
+    								scoring=config.config.metric, 
+    								cv=config.config.base_cv, 
+    								n_jobs=-1, 
+    								verbosity=1).fit(X_train[0:10000,:], y_train[0:10000]).fitted_pipeline_)
+
+# go into a loop for this one!
+for _ in range(num_hidden_layers):
+
+	# just the list of pipelines for this layer
+	hidden_list = []
+
+	# and we need to choose the number we're going to have for this layer
+	num_hidden = random.randint(config.config.num_hidden[0], config.config.num_hidden[1])
+
+	print()
+	print('Training {} hidden TPOT pipelines'.format(num_hidden))
+	for i in range(num_hidden):
+	    
+	    hidden_list.append(TPOTClassifier(generations=config.config.hidden_num_gens, 
+	    									population_size=config.config.hidden_pop_size, 
+	    									scoring=config.config.metric, 
+	    									cv=config.config.hidden_cv, 
+	    									n_jobs=-1, 
+	    									verbosity=1).fit(X_train[10000:20000,:], y_train[10000:20000]).fitted_pipeline_)
+
+	# then when we're all done we'll append this whole layer to the hidden_lol
+	hidden_lol.append(hidden_list)
+
+ens = SuperLearner(verbose=2, folds=config.config.num_folds)
+
+ens.add(base_list)
+
+# for the hidden lists we'll need a loop
+for hidden_list in hidden_lol:
+	ens.add(hidden_list)
+
+# fit this and grab the predictions!
+##### CONCERN THAT WE NEED TO DO THIS OVER MULTIPLE FOLDS. I'M NOT SURE IF THIS HAPPENS AUTOMATICALLY
+ens.fit(X_train, y_train)
+hidden_preds = ens.predict(X_test)
+
+voting_list = []
+
+for _ in range(config.config.num_voters):
+
+	voting_list.append(TPOTClassifier(generations=config.config.voting_num_gens, 
+										population_size=config.config.voting_pop_size, 
+										cv=config.config.voting_cv, 
+										scoring=config.config.metric, 
+										n_jobs=-1, 
+										verbosity=2).train(hidden_preds, y_train).fitted_pipeline_)
+
+ens.add_meta(VotingClassifier(voting_list))
+
+ens.fit(X_train, y_train)
+print()
+print('Overall score = {}'.format(error(ens.predict(X_test), y_test)))
 
 
-hidden_list = []
-for i in range(10):
+def error(preds, y_test):
     
-    hidden_list.append(TPOTClassifier(generations=2, population_size=2, scoring="f1", cv=2, n_jobs=-1, verbosity=1).fit(X_train[10000:20000,:], y_train[10000:20000]).fitted_pipeline_)
+    error_name = config.config.metric
+    
+    if error_name == 'roc_auc':
+        return roc_auc_score(preds, y_test)
+    elif error_name == 'accuracy':
+        return accuracy_score(preds, y_test)
+    elif error_name == 'recall':
+        return recall_score(preds, y_test)
+    elif error_name == 'precision':
+        return precision_score(preds, y_test)
+    elif error_name == 'f1':
+        return f1_score(preds, y_test)
+    else:
+        print('unsure what your metric is in the config so using accuracy instead')
+        return accuracy_score(preds, y_test)
 
-model = SuperLearner(verbose=2, folds=2)
+# yay! A function to help us select features based on correlation :)
+def feature_selection(feature_matrix, missing_threshold=90, correlation_threshold=0.95):
+    """Feature selection for a dataframe."""
+    
+    feature_matrix = pd.get_dummies(feature_matrix)
+    n_features_start = feature_matrix.shape[1]
+    print('Original shape: ', feature_matrix.shape)
 
-print('adding the base list to the super learner')
-model.add(base_list)
-model.add(hidden_list)
+    # Find missing and percentage
+    missing = pd.DataFrame(feature_matrix.isnull().sum())
+    missing['percent'] = 100 * (missing[0] / feature_matrix.shape[0])
+    missing.sort_values('percent', ascending = False, inplace = True)
 
-print('adding meta learner')
-model.add_meta(TPOTClassifier(generations=2, population_size=5, cv=5, scoring="f1", n_jobs=-1, verbosity=2))
+    # Missing above threshold
+    missing_cols = list(missing[missing['percent'] > missing_threshold].index)
+    n_missing_cols = len(missing_cols)
 
-print('fitting super learner!')
-model.fit(X_train, y_train)
-print('score = {}'.format(f1_score(model.predict(X_test), y_test)))
- 
+    # Remove missing columns
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in missing_cols]]
+    print('{} missing columns with threshold: {}.'.format(n_missing_cols,
+                                                                        missing_threshold))
+    
+    # Zero variance
+    unique_counts = pd.DataFrame(feature_matrix.nunique()).sort_values(0, ascending = True)
+    zero_variance_cols = list(unique_counts[unique_counts[0] == 1].index)
+    n_zero_variance_cols = len(zero_variance_cols)
+
+    # Remove zero variance columns
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in zero_variance_cols]]
+    print('{} zero variance columns.'.format(n_zero_variance_cols))
+    
+    # Correlations
+    corr_matrix = feature_matrix.corr()
+
+    # Extract the upper triangle of the correlation matrix
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
+
+    # Select the features with correlations above the threshold
+    # Need to use the absolute value
+    to_drop = [column for column in upper.columns if any(upper[column].abs() > correlation_threshold)]
+
+    n_collinear = len(to_drop)
+    
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in to_drop]]
+    print('{} collinear columns removed with threshold: {}.'.format(n_collinear,
+                                                                          correlation_threshold))
+    
+    total_removed = n_missing_cols + n_zero_variance_cols + n_collinear
+    
+    print('Total columns removed: ', total_removed)
+    print('Shape after feature selection: {}.'.format(feature_matrix.shape))
+    return feature_matrix
+
+
+
 
