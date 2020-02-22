dbbee84f345ab9b59d782b8465729f7889988cec
Michael Langford <michael.langford29@gmail.com>
2020-02-21 18:19:16 -0600

    putting the functions up first

--- tpot_ensemble_test.py
+++ tpot_ensemble_test.py
@@ -37,6 +37,80 @@ def warn(*args, **kwargs):
 import warnings
 warnings.warn = warn
 
+# generic error function
+def error(preds, y_test):
+    
+    error_name = config.config['metric']
+    
+    if error_name == 'roc_auc':
+        return roc_auc_score(preds, y_test)
+    elif error_name == 'accuracy':
+        return accuracy_score(preds, y_test)
+    elif error_name == 'recall':
+        return recall_score(preds, y_test)
+    elif error_name == 'precision':
+        return precision_score(preds, y_test)
+    elif error_name == 'f1':
+        return f1_score(preds, y_test)
+    else:
+        print('unsure what your metric is in the config so using accuracy instead')
+        return accuracy_score(preds, y_test)
+
+# yay! A function to help us select features based on correlation :)
+def feature_selection(feature_matrix, missing_threshold=90, correlation_threshold=0.95):
+    """Feature selection for a dataframe."""
+    
+    feature_matrix = pd.get_dummies(feature_matrix)
+    n_features_start = feature_matrix.shape[1]
+    print('Original shape: ', feature_matrix.shape)
+
+    # Find missing and percentage
+    missing = pd.DataFrame(feature_matrix.isnull().sum())
+    missing['percent'] = 100 * (missing[0] / feature_matrix.shape[0])
+    missing.sort_values('percent', ascending = False, inplace = True)
+
+    # Missing above threshold
+    missing_cols = list(missing[missing['percent'] > missing_threshold].index)
+    n_missing_cols = len(missing_cols)
+
+    # Remove missing columns
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in missing_cols]]
+    print('{} missing columns with threshold: {}.'.format(n_missing_cols,
+                                                                        missing_threshold))
+    
+    # Zero variance
+    unique_counts = pd.DataFrame(feature_matrix.nunique()).sort_values(0, ascending = True)
+    zero_variance_cols = list(unique_counts[unique_counts[0] == 1].index)
+    n_zero_variance_cols = len(zero_variance_cols)
+
+    # Remove zero variance columns
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in zero_variance_cols]]
+    print('{} zero variance columns.'.format(n_zero_variance_cols))
+    
+    # Correlations
+    corr_matrix = feature_matrix.corr()
+
+    # Extract the upper triangle of the correlation matrix
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
+
+    # Select the features with correlations above the threshold
+    # Need to use the absolute value
+    to_drop = [column for column in upper.columns if any(upper[column].abs() > correlation_threshold)]
+
+    n_collinear = len(to_drop)
+    
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in to_drop]]
+    print('{} collinear columns removed with threshold: {}.'.format(n_collinear,
+                                                                          correlation_threshold))
+    
+    total_removed = n_missing_cols + n_zero_variance_cols + n_collinear
+    
+    print('Total columns removed: ', total_removed)
+    print('Shape after feature selection: {}.'.format(feature_matrix.shape))
+    return feature_matrix
+
+
+# finally let's import the data
 df = pd.read_csv("creditcard.csv")
 df = df.drop(['Time'], axis=1) #,'V28','V27','V26','V25','V24','V23','V22','V20','V15','V13','V8'], axis =1)
 df = df.dropna()
@@ -198,76 +272,7 @@ print()
 print('Overall score = {}'.format(error(ens.predict(X_test), y_test)))
 
 
-def error(preds, y_test):
-    
-    error_name = config.config['metric']
-    
-    if error_name == 'roc_auc':
-        return roc_auc_score(preds, y_test)
-    elif error_name == 'accuracy':
-        return accuracy_score(preds, y_test)
-    elif error_name == 'recall':
-        return recall_score(preds, y_test)
-    elif error_name == 'precision':
-        return precision_score(preds, y_test)
-    elif error_name == 'f1':
-        return f1_score(preds, y_test)
-    else:
-        print('unsure what your metric is in the config so using accuracy instead')
-        return accuracy_score(preds, y_test)
 
-# yay! A function to help us select features based on correlation :)
-def feature_selection(feature_matrix, missing_threshold=90, correlation_threshold=0.95):
-    """Feature selection for a dataframe."""
-    
-    feature_matrix = pd.get_dummies(feature_matrix)
-    n_features_start = feature_matrix.shape[1]
-    print('Original shape: ', feature_matrix.shape)
-
-    # Find missing and percentage
-    missing = pd.DataFrame(feature_matrix.isnull().sum())
-    missing['percent'] = 100 * (missing[0] / feature_matrix.shape[0])
-    missing.sort_values('percent', ascending = False, inplace = True)
-
-    # Missing above threshold
-    missing_cols = list(missing[missing['percent'] > missing_threshold].index)
-    n_missing_cols = len(missing_cols)
-
-    # Remove missing columns
-    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in missing_cols]]
-    print('{} missing columns with threshold: {}.'.format(n_missing_cols,
-                                                                        missing_threshold))
-    
-    # Zero variance
-    unique_counts = pd.DataFrame(feature_matrix.nunique()).sort_values(0, ascending = True)
-    zero_variance_cols = list(unique_counts[unique_counts[0] == 1].index)
-    n_zero_variance_cols = len(zero_variance_cols)
-
-    # Remove zero variance columns
-    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in zero_variance_cols]]
-    print('{} zero variance columns.'.format(n_zero_variance_cols))
-    
-    # Correlations
-    corr_matrix = feature_matrix.corr()
-
-    # Extract the upper triangle of the correlation matrix
-    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
-
-    # Select the features with correlations above the threshold
-    # Need to use the absolute value
-    to_drop = [column for column in upper.columns if any(upper[column].abs() > correlation_threshold)]
-
-    n_collinear = len(to_drop)
-    
-    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in to_drop]]
-    print('{} collinear columns removed with threshold: {}.'.format(n_collinear,
-                                                                          correlation_threshold))
-    
-    total_removed = n_missing_cols + n_zero_variance_cols + n_collinear
-    
-    print('Total columns removed: ', total_removed)
-    print('Shape after feature selection: {}.'.format(feature_matrix.shape))
-    return feature_matrix
 
 
 
