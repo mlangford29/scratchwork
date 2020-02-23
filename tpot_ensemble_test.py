706e4c766eb2dbc21299efd818d77d390130042b
Michael Langford <michael.langford29@gmail.com>
2020-02-22 19:26:37 -0600

    typo

--- tpot_ensemble_test.py
+++ tpot_ensemble_test.py
@@ -224,7 +224,7 @@ for _ in range(num_base):
     								scoring=config.config['metric'], 
     								cv=config.config['base_cv'], 
     								n_jobs=-1,
-    								config_dict=config['base_models'],
+    								config_dict=config.base_models,
     								verbosity=1).fit(X_train[0:5000,:], y_train[0:5000]).fitted_pipeline_)
 
 # go into a loop for this one!
