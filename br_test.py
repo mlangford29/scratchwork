e866d9499bb78dfa4be665f333f9dee9d7650b19
Michael Langford <michael.langford29@gmail.com>
2020-02-25 18:17:43 -0600

    ugh3

--- br_test.py
+++ br_test.py
@@ -14,6 +14,59 @@ df = pd.read_csv("creditcard.csv")
 df = df.drop(['Time'], axis=1) #,'V28','V27','V26','V25','V24','V23','V22','V20','V15','V13','V8'], axis =1)
 df = df.dropna()
 
+
+def feature_selection(feature_matrix, missing_threshold=90, correlation_threshold=0.95):
+    """Feature selection for a dataframe."""
+    
+    feature_matrix = pd.get_dummies(feature_matrix)
+    n_features_start = feature_matrix.shape[1]
+    print('Original shape: ', feature_matrix.shape)
+
+    # Find missing and percentage
+    missing = pd.DataFrame(feature_matrix.isnull().sum())
+    missing['percent'] = 100 * (missing[0] / feature_matrix.shape[0])
+    missing.sort_values('percent', ascending = False, inplace = True)
+
+    # Missing above threshold
+    missing_cols = list(missing[missing['percent'] > missing_threshold].index)
+    n_missing_cols = len(missing_cols)
+
+    # Remove missing columns
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in missing_cols]]
+    print('{} missing columns with threshold: {}.'.format(n_missing_cols,
+                                                                        missing_threshold))
+    
+    # Zero variance
+    unique_counts = pd.DataFrame(feature_matrix.nunique()).sort_values(0, ascending = True)
+    zero_variance_cols = list(unique_counts[unique_counts[0] == 1].index)
+    n_zero_variance_cols = len(zero_variance_cols)
+
+    # Remove zero variance columns
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in zero_variance_cols]]
+    print('{} zero variance columns.'.format(n_zero_variance_cols))
+    
+    # Correlations
+    corr_matrix = feature_matrix.corr()
+
+    # Extract the upper triangle of the correlation matrix
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k = 1).astype(np.bool))
+
+    # Select the features with correlations above the threshold
+    # Need to use the absolute value
+    to_drop = [column for column in upper.columns if any(upper[column].abs() > correlation_threshold)]
+
+    n_collinear = len(to_drop)
+    
+    feature_matrix = feature_matrix[[x for x in feature_matrix if x not in to_drop]]
+    print('{} collinear columns removed with threshold: {}.'.format(n_collinear,
+                                                                          correlation_threshold))
+    
+    total_removed = n_missing_cols + n_zero_variance_cols + n_collinear
+    
+    print('Total columns removed: ', total_removed)
+    print('Shape after feature selection: {}.'.format(feature_matrix.shape))
+    return feature_matrix
+
 # before we get into things, let's do all the featuretools definitions
 def log_plus_one(column):
 	return np.log(column + min(column) + 1)
@@ -58,6 +111,7 @@ feature_matrix, feature_names = ft.dfs(entityset=es, target_entity='obs',
 
 
 
+feature_matrix = feature_selection(feature_matrix, correlation_threshold = 0.95)
 
 df_ = feature_matrix # make a copy of this
 df_ = df_.dropna(how='any', axis=1)
