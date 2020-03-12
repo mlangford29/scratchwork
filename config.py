dbe66e0448d7c05425757650e80a2f3aa813539d
Michael Langford <michael.langford29@gmail.com>
2020-03-12 16:30:30 -0500

    conf

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ config = {
 	
 	# number of 'hidden' layers. Probably not the right term
 	# but these are the layers that are in between the base and the meta-learner
-	'num_hidden_layers':(2, 2),
+	'num_hidden_layers':(1, 1),
 
 	# number of models in the base. This is a range
-	'num_base':(50, 50),
+	'num_base':(10, 10),
 
 	# number of models in hidden layers. This is a range
 	'num_hidden':(10, 10),
