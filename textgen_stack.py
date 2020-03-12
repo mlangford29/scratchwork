36190160fd2cdcd175de335f55ea84c19196a1a5
Michael Langford <michael.langford29@gmail.com>
2020-03-12 09:06:21 -0500

    3

--- textgen_stack.py
+++ textgen_stack.py
@@ -71,7 +71,7 @@ char_indices = dict((c, i) for i, c in enumerate(chars))
 indices_char = dict((i, c) for i, c in enumerate(chars))
 
 # cut the text in semi-redundant sequences of maxlen characters
-maxlen = 2
+maxlen = 3
 step = 1
 sentences = []
 next_chars = []
