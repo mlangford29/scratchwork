eeec359fe4097294984ec460ca66d84c44054f30
Michael Langford <michael.langford29@gmail.com>
2020-03-21 17:54:32 -0500

    even fewer features

--- tpot_ensemble_test.py
+++ tpot_ensemble_test.py
@@ -329,9 +329,8 @@ es = es.entity_from_dataframe(dataframe = df.drop('Class', axis=1),
 
 feature_matrix, feature_names = ft.dfs(entityset=es, target_entity='obs',
 										agg_primitives = ['min', 'max', 'mean', 'count', 'sum', 'std', 'trend'],
-										trans_primitives = ['haversine', 'multiply_numeric', 'diff', 'divide_by_feature', 
-															'divide_numeric','multiply_numeric_scalar', 'modulo_by_feature', 
-															'isin', 'absolute'],
+										trans_primitives = ['haversine', 'multiply_numeric', 'diff', 'multiply_numeric_scalar',
+															'absolute'],
 										max_depth=1,
 										n_jobs=1,
 										verbose=1)
