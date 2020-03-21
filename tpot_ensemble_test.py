12520b53e311d54f7d76c21075f4a73dbce888df
Michael Langford <michael.langford29@gmail.com>
2020-03-21 13:36:47 -0500

    AAA

--- tpot_ensemble_test.py
+++ tpot_ensemble_test.py
@@ -315,7 +315,7 @@ es = es.entity_from_dataframe(dataframe = df.drop('Class', axis=1),
 
 feature_matrix, feature_names = ft.dfs(entityset=es, target_entity='obs',
 										agg_primitives = ['min', 'max', 'mean', 'count', 'sum', 'std', 'trend'],
-										trans_primitives = ['percentile', b5, bnz],
+										trans_primitives = ['percentile', 'isin', 'cum_mean', 'subtract', 'divide'],
 										max_depth=1,
 										n_jobs=1,
 										verbose=1)
