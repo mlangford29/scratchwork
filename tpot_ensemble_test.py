f8d5b118a97508d22d7daadde2863c9e2171ec31
Michael Langford <michael.langford29@gmail.com>
2020-03-31 15:04:50 -0500

    aaaaa

--- tpot_ensemble_test.py
+++ tpot_ensemble_test.py
@@ -428,7 +428,7 @@ base_pred_df = pd.DataFrame()
 for i in range(num_base):
 
     # we need to make a dummy dataset
-    rand_weight_list = [random.rand() for i in range(len(list(br.keep_vars_)))]
+    rand_weight_list = [random.random() for i in range(len(list(br.keep_vars_)))]
     x_dummy, y_dummy = make_classification(n_features = len(list(br.keep_vars_), n_informative = random.randint(1, len(list(br.keep_vars_))), weights = rand_weight_list))
     
     base_list.append(TPOTClassifier(generations=config.config['base_num_gens'], 
