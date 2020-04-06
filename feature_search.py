703befa7e46fefd552f7639f4b19a641b16eeb9b
Michael Langford <michael.langford29@gmail.com>
2020-04-05 19:01:58 -0500

    wee"

--- feature_search.py
+++ feature_search.py
@@ -87,7 +87,7 @@ for t_prim in trans_primitive_list:
 	# if it is, then that means no additional feature was important
 	# if it's not (and there were additional important features)
 	#  then you need to add this to a list of trans primitives that you should use!
-	if len(list(ft_imp_dict.keys())) > 28:
+	if len(list(ft_imp_dict.keys())) > 29:
 		useful_prim_list.append(t_prim)
 
 	# now you have a dictionary of the importances from that single trans primitive
