b5b384587094b574deeb0439a6153b39085def8e
Michael Langford <michael.langford29@gmail.com>
2020-04-06 15:20:06 -0500

    len feature list

--- tpot_ensemble_test.py
+++ tpot_ensemble_test.py
@@ -419,7 +419,7 @@ for i in range(num_base):
 
     # we need to make a dummy dataset
     rand_weight_list = [random.random(), random.random()]
-    x_dummy, y_dummy = make_classification(n_features = len(list(br.keep_vars_)), n_informative = random.randint(2, 5), weights = rand_weight_list)
+    x_dummy, y_dummy = make_classification(n_features = len(feature_list), n_informative = random.randint(2, 5), weights = rand_weight_list)
     
     base_list.append(TPOTClassifier(generations=config.config['base_num_gens'], 
     								population_size=config.config['base_pop_size'], 
